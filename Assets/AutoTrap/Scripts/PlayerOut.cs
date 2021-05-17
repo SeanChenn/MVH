44aa1e275bb39715eb84ab013717bd0409623954
SeanChenn <48009686+SeanChenn@users.noreply.github.com>
2021-05-17 22:26:02 +0800

    AutoTrap

---
+++ Assets/AutoTrap/Scripts/PlayerOut.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AttackerPlayer
+{
+    public class PlayerOut : MonoBehaviour
+    {
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                other.transform.localPosition = new Vector3(0, 1.5f, -45);
+                Debug.Log("掉出界");
+            }
+        }
+    }
+}
+
---
+++ Assets/AutoTrap/Scripts/Trap1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AttackerPlayer
+{
+    public class Trap1 : MonoBehaviour
+    {
+        [SerializeField] float fSpeed;
+        float fRotateZ;
+        Vector3 v3Rotate;
+        [SerializeField] bool isTurnLeft;
+        // Start is called before the first frame update
+        void Start()
+        {
+            v3Rotate = transform.eulerAngles;
+            fRotateZ = transform.eulerAngles.z;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            TrapRotate();
+        }
+        void TrapRotate()
+        {
+            if (fRotateZ <= -60)
+            {
+                isTurnLeft = true;
+            }
+            else if (fRotateZ >= 60)
+            {
+                isTurnLeft = false;
+            }
+            if (isTurnLeft)
+            {
+                fRotateZ += fSpeed * Time.deltaTime;
+            }
+            else if (!isTurnLeft)
+            {
+                fRotateZ -= fSpeed * Time.deltaTime;
+            }
+
+            v3Rotate.z = fRotateZ;
+            transform.eulerAngles = v3Rotate;
+            //Debug.Log($"v3Rotate : {v3Rotate}");
+        }
+    }
+}
